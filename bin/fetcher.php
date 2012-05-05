b2790a91ea006e7bc60c76ba676025adca94403b
Chris Pollett <chris@pollett.org>
2012-05-05 15:52:38 -0700

    Bumping memory requirements of fetcher again, a=chris

--- bin/fetcher.php
+++ bin/fetcher.php
@@ -42,7 +42,7 @@ define("BASE_DIR", substr(
     dirname(realpath($_SERVER['PHP_SELF'])), 0, 
     -strlen("/bin")));
 
-ini_set("memory_limit","1000M"); //so have enough memory to crawl big pages
+ini_set("memory_limit","1100M"); //so have enough memory to crawl big pages
 
 /** Load in global configuration settings */
 require_once BASE_DIR.'/configs/config.php';
