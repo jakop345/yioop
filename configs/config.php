fff6e4a23f7b601242822b0c552b35c8d708284b
epinapala <epinapala@ebay.com>
2014-10-07 21:00:29 -0700

    revert config.php

--- configs/config.php
+++ configs/config.php
@@ -100,7 +100,7 @@ if(MAINTENANCE_MODE && $_SERVER["SERVER_ADDR"] != $_SERVER["REMOTE_ADDR"]) {
 if(!defined('WORK_DIRECTORY')) {
 /*+++ The next block of code is machine edited, change at 
 your own risk, please use configure web page instead +++*/
-define('WORK_DIRECTORY', '/Applications/XAMPP/htdocs/yioop_data');
+define('WORK_DIRECTORY', '');
 /*++++++*/
 // end machine edited code
 }
